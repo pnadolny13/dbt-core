0200f73b6bb09fbfb89f8a4b1fab791e8c92569a
Doug Beatty <doug.beatty@dbtlabs.com>
2024-07-22 13:29:23 -0600

    Clarify that the first argument is positional-only

--- core/dbt/cli/main.py
+++ core/dbt/cli/main.py
@@ -7,7 +7,6 @@ import click
 from click.exceptions import BadOptionUsage
 from click.exceptions import Exit as ClickExit
 from click.exceptions import NoSuchOption, UsageError
-
 from dbt.artifacts.schemas.catalog import CatalogArtifact
 from dbt.artifacts.schemas.run import RunExecutionResult
 from dbt.cli import params as p
@@ -47,7 +46,7 @@ class dbtRunner:
             callbacks = []
         self.callbacks = callbacks
 
-    def invoke(self, args: List[str], **kwargs) -> dbtRunnerResult:
+    def invoke(self, args: List[str], /, **kwargs) -> dbtRunnerResult:
         try:
             dbt_ctx = cli.make_context(cli.name, args.copy())
             dbt_ctx.obj = {
