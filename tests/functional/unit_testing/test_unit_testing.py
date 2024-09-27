924ce2d36a9083582b64a3fc0b24e6ac45aef6eb
Doug Beatty <44704949+dbeatty10@users.noreply.github.com>
2024-09-27 09:27:44 -0600

    Fix unit tests for incremental models with alias (#10755) (#10769)
    (cherry picked from commit a8d4ba2b4a04d76ad03e563aefd1fcf3ba86fe00)
    
    Co-authored-by: Katsuya Shimabukuro <katsu.generation.888@gmail.com>
    Co-authored-by: Gerda Shank <gerda@dbtlabs.com>

--- tests/functional/unit_testing/test_unit_testing.py
+++ tests/functional/unit_testing/test_unit_testing.py
@@ -21,6 +21,8 @@ from fixtures import (  # noqa: F401
     test_my_model_yml,
     datetime_test,
     my_incremental_model_sql,
+    my_incremental_model_versioned_yml,
+    my_incremental_model_with_alias_sql,
     event_sql,
     test_my_model_incremental_yml_basic,
     test_my_model_yml_invalid,
@@ -259,6 +261,42 @@ unit_tests:
       rows:
         - {id: 1, c: 7}
 """
+
+
+class TestUnitTestIncrementalModelWithAlias:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_incremental_model.sql": my_incremental_model_with_alias_sql,
+            "events.sql": event_sql,
+            "schema.yml": test_my_model_incremental_yml_basic,
+        }
+
+    def test_basic(self, project):
+        results = run_dbt(["run"])
+        assert len(results) == 2
+
+        # Select by model name
+        results = run_dbt(["test", "--select", "my_incremental_model"], expect_pass=True)
+        assert len(results) == 2
+
+
+class TestUnitTestIncrementalModelWithVersion:
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_incremental_model.sql": my_incremental_model_sql,
+            "events.sql": event_sql,
+            "schema.yml": my_incremental_model_versioned_yml + test_my_model_incremental_yml_basic,
+        }
+
+    def test_basic(self, project):
+        results = run_dbt(["run"])
+        assert len(results) == 2
+
+        # Select by model name
+        results = run_dbt(["test", "--select", "my_incremental_model"], expect_pass=True)
+        assert len(results) == 2
 
 
 class TestUnitTestExplicitSeed:
